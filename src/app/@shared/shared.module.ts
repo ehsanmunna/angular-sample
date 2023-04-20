2c02f8d3cdd80c089626ebee8054c973ca50e159
Ehsan Munna <ehsanul.munna@surbanajurong.com>
2023-04-21 05:19:46 +0600

    exportable module into shared module.

--- src/app/@shared/shared.module.ts
+++ src/app/@shared/shared.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UtilityModule } from '../@utility/utility.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-
+const EXPORTABLE_MODULE = [
+  FormsModule,
+  ReactiveFormsModule,
+];
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
+    ...EXPORTABLE_MODULE,
     UtilityModule
   ],
   exports: [
+    ...EXPORTABLE_MODULE,
     UtilityModule
   ]
 })
